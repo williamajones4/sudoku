372b06645269b14d2c7de813ab95601541e1bf8a
Alex <william.a.jones4@gmail.com>
2025-03-13 01:03:35 -0500

    added in some more init and test things

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-coverage run
+coverage run -m unittest discover
 coverage report
--- test/test.py
+++ test/test.py
@@ -1,10 +1,19 @@
 """These are the unit tests for main"""
 
+import unittest
 
-def test_possibilities():
-    "Unit test for possibilities test"
-    print("pass")
+
+class test_possibilities(unittest.Testcase):
+    "Test Object for unit test creation"
+
+    def test_possibilities(self):
+        "Unit test for possibilities test"
+        self.assertEqual(1, 1)
+
+
+def main():
+    unittest.main()
 
 
 if __name__ == "__main__":
-    test_possibilities()
+    main()
