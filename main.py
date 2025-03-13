f77c55e4c7d5551082b2f728e87ad2c1f14ca6a4
Alex <william.a.jones4@gmail.com>
2025-03-13 00:16:26 -0500

    stubbed out the possibilities function and test

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ def main():
     print("This is it")
 
 
+def possibilities_finder(array):
+    """
+    Desc: This function finds the possibilities left in an array
+
+    Args: array - list that you want to check
+
+    Return: result - list of possibilities
+    """
+    print(array)
+    result = []
+    return result
+
+
 if __name__ == "__main__":
     main()
---
+++ test/test.py
@@ -0,0 +1,10 @@
+"""These are the unit tests for main"""
+
+
+def test_possibilities():
+    "Unit test for possibilities test"
+    print("pass")
+
+
+if __name__ == "__main__":
+    test_possibilities()
