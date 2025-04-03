144854603ad6f2af1390e78d3052a348a437f23d
Alex <william.a.jones4@gmail.com>
2025-04-03 01:01:21 -0500

    added app.py to start a basic level web app

---
+++ app.py
@@ -0,0 +1,28 @@
+"This is a simple application file to be run with Dash library"
+
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+import visualize
+
+app = Dash()
+
+# Requires Dash 2.17.0 or later
+app.layout = [
+    html.H1(children='Sudoku Solver App', style={'textAlign':'center'}),
+    html.Div([
+        dcc.Upload(html.Button('Upload File'), id='upload_puzzle')
+    ]),
+    html.Div(id='plot')
+]
+
+@callback(
+    Output('plot', 'figure'),
+    Input('upload_puzzle', 'contents')
+)
+def show_problem(puzzle):
+    figure = visualize.visualize_sudoku(puzzle)
+    return figure
+
+if __name__ == '__main__':
+    app.run(debug=True)
